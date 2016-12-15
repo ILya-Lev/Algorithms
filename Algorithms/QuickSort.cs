8e5bc62fab447d41216ac92789137e9d7f82d103
ILya-Lev <lis2889@gmail.com>
2016-12-15 12:15:19 +0200

    added Quick Sort with random initial element complaxity O(n*log(n)) where n - amount of elements in input sequence

---
+++ Algorithms/QuickSort.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public static class QuickSortUtility
+	{
+		public static IList<T> QuickSort<T>(this IList<T> source) where T : IComparable<T>
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+
+			var sequence = new T[source.Count];
+			source.CopyTo(sequence, 0);
+
+			QuickSortImpl<T>.SortAroundPivot(sequence, 0, sequence.Length);
+			return sequence;
+		}
+
+		private static class QuickSortImpl<T> where T : IComparable<T>
+		{
+			private static readonly Random _rand = new Random(DateTime.UtcNow.Millisecond);
+
+			public static void SortAroundPivot(IList<T> source, int begin, int end)
+			{
+				if (begin >= end) return;
+
+				if (begin < end - 1)
+					Swap(source, begin, ChoosePivot(begin, end - 1));
+				var pivotIdx = begin;
+
+				var start = begin + 1;
+				var finish = end;
+				for (int i = start; i < finish; i++)
+				{
+					var compare = source[pivotIdx].CompareTo(source[i]);
+					if (compare < 0)
+					{
+						Swap(source, i, finish - 1);
+						finish--;
+						i--;    // we do not need to increase i in this iteration
+					}
+					else if (compare > 0)
+					{
+						Swap(source, i, pivotIdx);
+						pivotIdx++;
+					}
+				}
+
+				SortAroundPivot(source, begin, pivotIdx);
+				SortAroundPivot(source, pivotIdx + 1, end);
+			}
+
+			private static int ChoosePivot(int begin, int end) => _rand.Next(begin, end - 1);
+
+			private static void Swap(IList<T> source, int lhs, int rhs)
+			{
+				var tmp = source[lhs];
+				source[lhs] = source[rhs];
+				source[rhs] = tmp;
+			}
+		}
+	}
+}
---
+++ UnitTests/QuickSortUnitTests.cs
@@ -0,0 +1,85 @@
+﻿using Algorithms;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Linq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class QuickSortUnitTests
+	{
+		[TestMethod]
+		public void DescendingToAscending_Sort()
+		{
+			var data = new[] { 10, 9, 8, 7, 6, 5, 4, 3, 2, 1 };
+
+			var sorted = data.QuickSort();
+
+			sorted.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		[ExpectedException(typeof(ArgumentNullException), "there should be an exception!")]
+		public void NullSource_ShouldThrowException()
+		{
+			int[] data = null;
+
+			var sorted = data.QuickSort();
+		}
+
+		public void RandomDataSorter(int limit)
+		{
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			var data = Enumerable.Range(1, limit).ToList();
+			for (int i = 0; i < limit; i++)
+			{
+				var idx = rand.Next(0, limit - 1);
+				data[i] = data[i] ^ data[idx];
+				data[idx] = data[i] ^ data[idx];
+				data[i] = data[i] ^ data[idx];
+			}
+
+			var sorted = data.QuickSort();
+
+			sorted.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		public void Random10Data_ShouldSort()
+		{
+			RandomDataSorter(10);
+		}
+		[TestMethod]
+		public void Random100Data_ShouldSort()
+		{
+			RandomDataSorter(100);
+		}
+		[TestMethod]
+		public void Random1000Data_ShouldSort()
+		{
+			RandomDataSorter(1000);
+		}
+		[TestMethod]
+		public void Random10_7Data_ShouldSort()
+		{
+			RandomDataSorter(10000000);
+		}
+		[TestMethod]
+		public void Random10_7Data_LinqSort()
+		{
+			var limit = 10000000;
+			var rand = new Random(DateTime.UtcNow.Millisecond);
+			var data = Enumerable.Range(1, limit).ToList();
+			for (int i = 0; i < limit; i++)
+			{
+				var idx = rand.Next(0, limit - 1);
+				data[i] = data[i] ^ data[idx];
+				data[idx] = data[i] ^ data[idx];
+				data[i] = data[i] ^ data[idx];
+			}
+
+			var sorted = data.AsParallel().OrderBy(item => item);
+
+			sorted.Should().BeInAscendingOrder();
+		}
+	}
+}
