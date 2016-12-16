81e2b954c75ac9ca592c1cb7867e24172f026b73
ILya-Lev <lis2889@gmail.com>
2016-12-16 19:04:46 +0200

    added scheduling via brute force added permutations generation via recursion algorithm (is utilized in brute force one) adequate implementation pending

---
+++ Algorithms/SchedulingWeighted.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace Algorithms
+{
+	public static class SchedulingWeighted
+	{
+		public static IList<Job> OrderJobs(IList<Job> source) =>
+			//sequence.OrderBy(job => job.Length)
+			//		.ThenByDescending(job => job.Weight)
+			source.OrderByDescending(job => job.Weight / job.Length)
+					.ToList();
+
+		public static int ScoreViaBruteForce(IList<Job> source)
+		{
+			var permutations = GeneratePermutations(source).ToList();
+			var scoredPermutations = permutations.Select(p => new
+			{
+				Score = OverallCompletionScore(p),
+				Permutation = p
+			})
+			.OrderBy(item => item.Score)
+			.ToList();
+
+			return scoredPermutations.First().Score;
+		}
+
+		private static int OverallCompletionScore(IList<Job> source)
+		{
+			var score = 0;
+			int completionTime = 0;
+			foreach (var job in source)
+			{
+				completionTime += job.Length;
+				score += completionTime * job.Weight;
+			}
+			return score;
+		}
+
+		public static IEnumerable<IList<T>> GeneratePermutations<T>(IList<T> source, int begin = 0)
+		{
+			if (begin >= source.Count)
+				yield return source;
+
+			for (int pivot = begin; pivot < source.Count; pivot++)
+			{
+				var currentPermutation = new List<T>(source);
+				Swap(currentPermutation, begin, pivot);
+				//yield return currentPermutation;
+
+				var nextPermutation = GeneratePermutations(currentPermutation, begin + 1);
+				foreach (var permutation in nextPermutation)
+				{
+					yield return permutation;
+				}
+			}
+		}
+
+		private static void Swap<T>(IList<T> previousStep, int begin, int pivot)
+		{
+			var tmp = previousStep[begin];
+			previousStep[begin] = previousStep[pivot];
+			previousStep[pivot] = tmp;
+		}
+	}
+
+	[DebuggerDisplay("L = {Length}; W = {Weight}")]
+	public class Job
+	{
+		public int Length { get; set; }
+		public int Weight { get; set; }
+	}
+}
---
+++ UnitTests/SchedulingWeightedUnitTests.cs
@@ -0,0 +1,52 @@
+﻿using Algorithms;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class SchedulingWeightedUnitTests
+	{
+		[TestMethod]
+		public void OrderJobs_SimpleCase()
+		{
+			var data = new[]
+			{
+				new Job {Length = 3, Weight = 1},
+				new Job {Length = 2, Weight = 2},
+				new Job {Length = 1, Weight = 3},
+			};
+
+			var scheduledData = SchedulingWeighted.OrderJobs(data);
+
+			scheduledData.Select(job => job.Length)
+				.Should()
+				.BeInAscendingOrder("for the case optimal solution is one with length ordered as 1,2,3");
+		}
+		[TestMethod]
+		public void ScoreViaBruteForce_SimpleCase()
+		{
+			var data = new[]
+			{
+				new Job {Length = 3, Weight = 1},
+				new Job {Length = 2, Weight = 2},
+				new Job {Length = 1, Weight = 3},
+			};
+
+			var score = SchedulingWeighted.ScoreViaBruteForce(data);
+
+			score.Should().Be(15);
+		}
+
+		[TestMethod]
+		public void GeneratePermutations_SimpleCase()
+		{
+			var data = new[] { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j' };
+
+			var permutations = SchedulingWeighted.GeneratePermutations(data).Distinct().ToList();
+
+			permutations.Count.Should().Be(3628800, "permutations amount is a source sequence length factorial");
+		}
+	}
+}
