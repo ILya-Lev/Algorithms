393cccbc01277c3bc0c2fcaaf67955a13c5599fd
ILya-Lev <lis2889@gmail.com>
2016-12-19 17:17:37 +0200

    HeapOverList data structure added implementation of a heap which provides: O(1) access to min element (get) O(log n) insertion (for one element and nlog n for a range of elements) List<>.RemoveAt complexity for removing element by index and the min element too List<>.RemoveAll complexity for removing elements by predicate

---
+++ Algorithms/HeapOverList.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class HeapOverList<T> where T : IComparable<T>
+	{
+		internal readonly List<T> _storage = new List<T>();
+
+		public T Min => _storage.Count > 0 ? _storage[0] : default(T);
+		public int Count => _storage.Count;
+
+		public void AddRange(IEnumerable<T> sequence)
+		{
+			foreach (var item in sequence)
+				Add(item);
+		}
+
+		public void Add(T item) => FindRightPlace(item, 0, _storage.Count - 1);
+
+		private void FindRightPlace(T item, int begin, int end)
+		{
+			if (begin > end)
+			{
+				_storage.Insert(begin, item);
+				return;
+			}
+
+			var middle = (end + begin) / 2;
+			var comparison = item?.CompareTo(_storage[middle]);
+			if (comparison == 0)
+			{
+				_storage.Insert(middle, item);
+				return;
+			}
+
+			if (comparison < 0)
+				FindRightPlace(item, begin, middle - 1);
+			else if (comparison > 0)
+				FindRightPlace(item, middle + 1, end);
+		}
+
+		public T RemoveMin() => RemoveAt(0);
+
+		public T RemoveAt(int index)
+		{
+			if (index < 0 || index >= _storage.Count)
+				throw new ArgumentOutOfRangeException(
+					$"Heap contains only {_storage.Count} elements. Cannot remove {index} item.");
+
+			var min = _storage[index];
+			_storage.RemoveAt(index);
+			return min;
+		}
+
+		public int RemoveBy(Predicate<T> predicate) => _storage.RemoveAll(predicate);
+	}
+}
---
+++ UnitTests/HeapOverListUnitTests.cs
@@ -0,0 +1,120 @@
+﻿using Algorithms;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class HeapOverListUnitTests
+	{
+		[TestMethod]
+		public void Add_DistinctValues_ShoulBeInAscendingOrder()
+		{
+			var heap = new HeapOverList<int>();
+			var data = new[] { 5, 8, 6, 3, 1, 9, 2 }.ToList();
+
+			data.ForEach(item => heap.Add(item));
+
+			heap._storage.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		public void Add_DuplicatedValues_ShoulBeInAscendingOrder()
+		{
+			var heap = new HeapOverList<int>();
+			var data = new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 }.ToList();
+
+			data.ForEach(item => heap.Add(item));
+
+			heap._storage.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		public void Add_NullValues_ShoulBeInAscendingOrder()
+		{
+			var heap = new HeapOverList<TestDataType>();
+			var data = new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 }
+						.Select((n, idx) => idx % 4 == 0
+										? default(TestDataType)
+										: new TestDataType { Value = n })
+						.ToList();
+
+			data.ForEach(item => heap.Add(item));
+
+			heap._storage.Should().BeInAscendingOrder();
+		}
+
+		private class TestDataType : IComparable<TestDataType>
+		{
+			public int Value { get; set; }
+			public int CompareTo(TestDataType other)
+			{
+				if (other == null)
+					return Comparer<TestDataType>.Default.Compare(this, null);
+				return Value.CompareTo(other.Value);
+			}
+		}
+
+		[TestMethod]
+		public void RemoveMin_ShouldReturnMinElement_DecreaseAmountByOne()
+		{
+			var heap = new HeapOverList<int>();
+			heap.AddRange(new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 });
+
+			var initMin = heap.Min;
+			var initCount = heap.Count;
+
+			var removedMin = heap.RemoveMin();
+
+			initMin.Should().Be(removedMin);
+			heap.Count.Should().Be(initCount - 1);
+			initMin.Should().BeLessOrEqualTo(heap.Min);
+			heap._storage.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		public void RemoveAtMiddle_ShouldReturnMiddleElement_DecreaseAmountByOne()
+		{
+			var heap = new HeapOverList<int>();
+			heap.AddRange(new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 });
+
+			var initMin = heap.Min;
+			var initCount = heap.Count;
+
+			var removedItem = heap.RemoveAt(heap.Count / 2);
+			Console.WriteLine($"removed item {removedItem}");
+
+			initMin.Should().Be(heap.Min);
+			heap.Count.Should().Be(initCount - 1);
+			heap._storage.Should().BeInAscendingOrder();
+		}
+		[TestMethod]
+		public void RemoveBy_Even_OnlyOddShouldStay()
+		{
+			var heap = new HeapOverList<int>();
+			heap.AddRange(new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 });
+
+			var initCount = heap.Count;
+
+			var removedCount = heap.RemoveBy(item => item % 2 == 0);
+
+			removedCount.Should().Be(initCount - heap.Count);
+			heap._storage.Should().BeInAscendingOrder();
+			heap._storage.Should().Contain(item => item % 2 == 1);
+		}
+		[TestMethod]
+		public void RemoveBy_Odd_OnlyEvenShouldStay()
+		{
+			var heap = new HeapOverList<int>();
+			heap.AddRange(new[] { 5, 8, 6, 3, 1, 5, 8, 6, 9, 9, 2 });
+
+			var initCount = heap.Count;
+
+			var removedCount = heap.RemoveBy(item => item % 2 == 1);
+
+			removedCount.Should().Be(initCount - heap.Count);
+			heap._storage.Should().BeInAscendingOrder();
+			heap._storage.Should().Contain(item => item % 2 == 0);
+		}
+	}
+}
