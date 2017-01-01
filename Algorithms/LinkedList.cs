57541823f6f6d3ed2300bc3b44b17960096d4f08
ILya-Lev <lis2889@gmail.com>
2017-01-01 14:46:03 +0200

    linked list primitive implementation with specific method in it - reverse and leave only items with even index

---
+++ Algorithms/LinkedList.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace Algorithms
+{
+	public class LinkedNode<T>
+	{
+		public T Data { get; set; }
+		public LinkedNode<T> Next { get; set; }
+	}
+
+	public class MyLinkedList<T> : IEnumerable<T>
+	{
+		public LinkedNode<T> Root { get; set; }
+
+		public static MyLinkedList<T> ReverseEven(MyLinkedList<T> source)
+		{
+			var evenNodes = new Stack<LinkedNode<T>>();
+			var current = source.Root;
+			while (current != null)
+			{
+				var next = current.Next;
+				if (next != null)
+					evenNodes.Push(next);
+				current = next?.Next;
+			}
+
+			if (evenNodes.Count == 0) return null;
+
+			var newRoot = evenNodes.Peek();
+			while (evenNodes.Count != 0)
+			{
+				current = evenNodes.Pop();
+				current.Next = evenNodes.Count != 0 ? evenNodes.Peek() : null;
+			}
+
+			return new MyLinkedList<T> { Root = newRoot };
+		}
+
+		public static MyLinkedList<T> ReverseEvenInPlace(MyLinkedList<T> source)
+		{
+			var even = source.Root.Next;
+			var odd = even.Next;
+			even.Next = null;
+			while (odd?.Next != null)
+			{
+				var tmp = even;
+				even = odd.Next;
+				odd = even.Next;
+				even.Next = tmp;
+			}
+			return new MyLinkedList<T> { Root = even };
+		}
+
+		public IEnumerator<T> GetEnumerator()
+		{
+			var current = Root;
+			while (current != null)
+			{
+				yield return current.Data;
+				current = current.Next;
+			}
+		}
+
+		IEnumerator IEnumerable.GetEnumerator()
+		{
+			return GetEnumerator();
+		}
+
+		public void AddItem(T data)
+		{
+			if (Root == null)
+			{
+				Root = new LinkedNode<T> { Data = data };
+				return;
+			}
+
+			var current = Root;
+			while (current.Next != null)
+			{
+				current = current.Next;
+			}
+			current.Next = new LinkedNode<T> { Data = data };
+		}
+	}
+}
---
+++ UnitTests/MyLinkedListUnitTests.cs
@@ -0,0 +1,44 @@
+﻿using Algorithms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class MyLinkedListUnitTests
+	{
+		[TestMethod]
+		public void ReverseEven_NormalCheck_ShouldReturnValidSequence()
+		{
+			var list = new MyLinkedList<int>();
+			for (int i = 0; i < 10; i++)
+			{
+				list.AddItem(i);
+			}
+
+			var evenList = MyLinkedList<int>.ReverseEven(list);
+
+			foreach (var item in evenList)
+			{
+				Console.Write(item + " ");
+			}
+		}
+
+		[TestMethod]
+		public void ReverseEvenInPlace_NormalCheck_ShouldReturnValidSequence()
+		{
+			var list = new MyLinkedList<int>();
+			for (int i = 0; i < 10; i++)
+			{
+				list.AddItem(i);
+			}
+
+			var evenList = MyLinkedList<int>.ReverseEvenInPlace(list);
+
+			foreach (var item in evenList)
+			{
+				Console.Write(item + " ");
+			}
+		}
+	}
+}
