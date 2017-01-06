0261e4bb04ed2b0588705775bfc2c896ba1418c8
ILya-Lev <lis2889@gmail.com>
2017-01-06 10:31:47 +0200

    added Buffer() extension method to return iterator for data type int[] of a given size from enumeration of int
    and a weird behavior for an iterator example

---
+++ Algorithms/EnumerableExtensions.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Algorithms
+{
+	public static class EnumerableExtensions
+	{
+		public static IEnumerable<int[]> Buffer(this IEnumerable<int> source, int size)
+		{
+			if (source == null)
+				throw new ArgumentNullException(nameof(source));
+			if (size <= 0)
+				throw new ArgumentOutOfRangeException(nameof(size), size,
+											"Buffer size should be larger than zero");
+
+			return BufferImpl(source, size);
+		}
+
+		private static IEnumerable<int[]> BufferImpl(IEnumerable<int> source, int size)
+		{
+			var buffer = new int[size];
+			var current = 0;
+
+			using (var iterator = source.GetEnumerator())
+			{
+				while (iterator.MoveNext())
+				{
+					buffer[current % size] = iterator.Current;
+					if (current % size == size - 1)
+						yield return buffer;
+					current++;
+				}
+			}
+
+			if (current % size != 0)
+				yield return buffer.Take(current % size).ToArray();
+		}
+	}
+}
---
+++ UnitTests/IteratorUnitTests.cs
@@ -0,0 +1,78 @@
+﻿using Algorithms;
+using FluentAssertions;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+namespace UnitTests
+{
+	[TestClass]
+	public class IteratorUnitTests
+	{
+		[TestMethod]
+		public void IteratorAsMutableStructure_Loop_WeirdBehavior()
+		{
+			var x = new { Items = new List<int> { 1, 2, 3, 4 }.GetEnumerator() };
+			while (x.Items.MoveNext())
+			{
+				Debug.WriteLine(x.Items);
+			}
+		}
+
+		[TestMethod]
+		public void Buffer_LengthIsDivisivleWithoutReminder_ShoulGoThroughAllElements()
+		{
+			var scale = 3;
+			var bufferSize = 7;
+			var length = bufferSize * scale;
+			var data = Enumerable.Range(1, length);
+
+			var steps = 0;
+			var seenElements = 0;
+
+			foreach (var bunch in data.Buffer(bufferSize))
+			{
+				steps++;
+				foreach (var i in bunch)
+				{
+					Debug.Write(i + " ");
+					seenElements++;
+				}
+				Debug.WriteLine(" ");
+			}
+
+			steps.Should()
+				.Be(scale
+				, $"source data is exactly '{nameof(scale)}'='{scale}' times larger than buffer");
+			seenElements.Should().Be(length, "should go through all elements of the source");
+		}
+
+		[TestMethod]
+		public void Buffer_LengthNotDivisivleWithoutReminder_ShoulGoThroughAllElements()
+		{
+			var scale = 3;
+			var bufferSize = 7;
+			var length = bufferSize * scale + bufferSize - 2;
+			var data = Enumerable.Range(1, length);
+
+			var steps = 0;
+			var seenElements = 0;
+
+			foreach (var bunch in data.Buffer(bufferSize))
+			{
+				steps++;
+				foreach (var i in bunch)
+				{
+					Debug.Write(i + " ");
+					seenElements++;
+				}
+				Debug.WriteLine(" ");
+			}
+
+			steps.Should()
+				.Be(scale + 1, $"source data is exactly '{nameof(scale)}' + 1 ='{scale + 1}' times larger than buffer");
+			seenElements.Should().Be(length, "should go through all elements of the source");
+		}
+	}
+}
